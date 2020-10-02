4442b8ad2818e9d2f6b1c05c7ccfc30b9dde749d
JinapriyaM <madushankakulasinghe@gmail.com>
2020-10-02 08:55:16 +0530

    update app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          YOUWRITE
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Under Development......
         </a>
       </header>
     </div>
